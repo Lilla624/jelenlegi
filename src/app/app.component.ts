5266eaab895c4a52d5812ffeef5ec038356c38ff
Lilla624 <vargalilla@ktch.hu>
2025-03-30 20:41:26 +0200

    reszponzivitas modositva

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'webshop';
+  screenSize: string = 'Asztali nézet';
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit() {
+    this.breakpointObserver.observe([
+      Breakpoints.Handset,   // Mobil
+      Breakpoints.Tablet,    // Tablet
+      Breakpoints.Web        // Asztali
+    ]).subscribe(result => {
+      if (result.matches) {
+        if (result.breakpoints[Breakpoints.Handset]) {
+          this.screenSize = 'Mobil nézet';
+        } else if (result.breakpoints[Breakpoints.Tablet]) {
+          this.screenSize = 'Tablet nézet';
+        } else if (result.breakpoints[Breakpoints.Web]) {
+          this.screenSize = 'Asztali nézet';
+        }
+      }
+      console.log('Aktuális nézet:', this.screenSize);
+    });
+  }
 }
